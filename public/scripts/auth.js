f936e36a13eb597804e5e839ad0a7c1661730144
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-05-24 01:13:39 -0400

    styles, logout and unlink

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,23 +1,39 @@
 (function($, amplify, window, undefined){
 	var $login;
+	var $logout;
+	var $unlink;
 
 	function init() {
 		getElements();
-		amplify.subscribe('ready', updateLink);
+		amplify.subscribe('ready', bindDOM);
 		amplify.subscribe('app:user', loadUser);
 	}
 
 	function getElements() {
 		$login = $('[login]');
+		$logout = $('[logout]');
+		$unlink = $('[unlink]');
 	}
 
-	function updateLink() {
-		$login.attr('href', '//' + __hsggServices.auth.domain + '/auth/bnet');
+	function bindDOM() {
+		$login.click(function() {
+			$login.replaceWith('Redirecting to Battle.Net...');
+			window.location = '//' + __hsggServices.auth.domain + '/auth/bnet';
+			return false;
+		});
+		$logout.click(function() {
+			window.location = 'http://us.battle.net/en/?logout';
+		});
+		$unlink.click(function() {
+			window.location = '//' + __hsggServices.auth.domain + '/unlink/bnet';
+		});
 	}
 
 	function loadUser(user) {
 		var str = JSON.stringify(user);
 		$login.replaceWith('<strong>Logged In</strong><pre>' + str + '</pre>');
+		$logout.show();
+		$unlink.show();
 	}
 
 	$(init);
