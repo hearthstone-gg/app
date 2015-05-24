68bc3e120f3904bb328ab1ffb97b6a37dc37fd50
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-05-23 21:30:08 -0400

    working user passing from the desktop app

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,10 +1,26 @@
 (function($, amplify, window, undefined){
+	var $login;
+
 	function init() {
+		getElements();
 		amplify.subscribe('ready', updateLink);
+		window.addEventListener("message", receiveMessage, false);
+	}
+
+	function getElements() {
+		$login = $('[login]');
 	}
 
 	function updateLink() {
-		$('[login]').attr('href', '//' + __hsggServices.auth.domain + '/auth/bnet');
+		$login.attr('href', '//' + __hsggServices.auth.domain + '/auth/bnet');
+	}
+
+	function receiveMessage(event) {
+		var user = JSON.stringify(event.data.data);
+		if (event.data.type === 'user') {
+			$login.replaceWith('<strong>Logged In</strong><pre>' + user + '</pre>');
+			amplify.publish('authed', user);
+		}
 	}
 
 	$(init);
--- public/scripts/socket.js
+++ public/scripts/socket.js
@@ -1,10 +1,12 @@
 (function($, amplify, window, undefined){
 	var socket;
+	var user;
 
 	function init() {
 		getElements();
 		bindDOM();
-		amplify.subscribe('ready', createSocket);
+		amplify.subscribe('authed', onAuth);
+		amplify.subscribe('ready', checkCreate);
 	}
 
 	function getElements() {
@@ -14,7 +16,18 @@
 	function bindDOM() {
 
 	}
+	function onAuth(u) {
+		user = u;
+		checkCreate();
+	}
+	function checkCreate() {
+		//make sure the user has been authed and the server config have been loaded
+		if (user && window.__hsggServices) {
+			createSocket();
+		}
+	}
 
+	//load socket.io
 	function createSocket() {
 		var script = document.createElement('script');
 		script.src = '//' + __hsggServices.socket.domain + '/socket.io/socket.io.js';
@@ -26,15 +39,17 @@
 		document.getElementsByTagName('head')[0].appendChild(script);
 	}
 
+	//bind it the socket stuff
 	function bindSocket() {
 		socket.on('connect', function() {
 			$('div[socket-log]').append('Connected to socket server<br />');
 		});
+		socket.emit('authed', user.token);
 		socket.on('disconnect', function() {
 			$('div[socket-log]').append('Disconnected from socket server<br />');
 		});
 		socket.on('ping', function() {
-			$('div[socket-log]').append('Ping from socket server<br />');
+			$('div[socket-log]').append('Ping from socket server to token room<br />');
 		});
 	}
 
